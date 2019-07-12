fbe7bbbb2bd6aeb1fd858094f63d16021debff30
Lucas Rosa Silverio <lucasrsilver@gmail.com>
2019-07-12 19:31:57 -0300

    termino da api

---
+++ app/Http/Controllers/MarcaController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Marca;
+
+class MarcaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $marca = new Marca();
+            $marca->nome = $request->input('nome');
+            $marca->descricao = $request->input('descricao');
+            $marca->save();
+            //return "Ok";
+        } catch (Exception $e) {
+            //return $e->getMessage();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function getAll() {
+        $marcas = Marca::all();
+        return json_encode($marcas);
+    }
+}
---
+++ app/Http/Controllers/VeiculoController.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Veiculo;
+use App\Marca;
+
+class VeiculoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        try {
+            $veiculo = new Veiculo();
+            $veiculo->ano_lancamento = $request->input('ano_lancamento');
+            $veiculo->marca = $request->input('marca');
+            $veiculo->descricao = $request->input('descricao');
+            $veiculo->tipo_veiculo = $request->input('tipo_veiculo');
+            $veiculo->imagem = $request->input('imagem');
+            $veiculo->save();
+            //return "Ok";
+        } catch (Exception $e) {
+            //return $e->getMessage();
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $veiculos = Veiculo::find($id);
+        $marca = Marca::find($veiculos->marca);
+        $veiculos["nome_marca"] = $marca->nome;
+        if (isset($veiculos)) {
+            return json_encode($veiculos);            
+        }
+        return response('Produto não encontrado', 404);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //$idveiculo = $request->input('id');
+        $veiculo = Veiculo::find($id);
+        if(isset($veiculo)){
+            $veiculo->ano_lancamento = $request->input('ano_lancamento');
+            $veiculo->marca = $request->input('marca');
+            $veiculo->descricao = $request->input('descricao');
+            $veiculo->tipo_veiculo = $request->input('tipo_veiculo');
+            $veiculo->imagem = $request->input('imagem');
+            $veiculo->save();
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $veiculos = Veiculo::find($id);
+        if (isset($veiculos)) {
+            $veiculos->delete();
+            return response('OK', 200);
+        }
+        return response('Veículo não encontrado', 404);
+    }
+
+    public function getAll() {
+        $veiculos = Veiculo::all();
+        $marcas = Marca::all();
+
+        foreach($veiculos as $veiculo) {
+            foreach($marcas as $marca) {
+                if($veiculo->marca == $marca->id) {
+                    $veiculo['nome_marca'] = $marca->nome;
+                }
+            }
+        }
+        return json_encode($veiculos);
+    }
+}
